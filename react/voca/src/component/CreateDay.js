4a41ac402c01e4e42bf5eff61e2c876bc1b6d6a4
mijeongkim3 <yellowredcandy3@gmail.com>
2024-05-05 15:09:45 +0900

    feat:24052

--- react/voca/src/component/CreateDay.js
+++ react/voca/src/component/CreateDay.js
@@ -2,7 +2,7 @@ import useFetch from '../hooks/useFetch';
 
 export default function CreateDay() {
   const days = useFetch("http://localhost:3001/days");
-  return (
+  return (//.
     <div>
       <h3> 현재 일수: {days.length} 일 </h3>
       <button> Day 추가</button>
